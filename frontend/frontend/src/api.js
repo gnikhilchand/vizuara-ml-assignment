ae7622d8f9289ccd15d47f279607698594e3da63
NIKHIL CHAND <61118151+gnikhilchand@users.noreply.github.com>
2025-12-23 16:57:58 +0530

    Update api.js

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+// const API_URL = 'http://localhost:8000';
+const API_URL = 'https://vizuara-backend-xyz.onrender.com';
 
 export const uploadDataset = async (file) => {
   const formData = new FormData();
